3b9885010057f2c91d093fdfeae94f1c7dede2e4
humphr18 <101128063+humphr18@users.noreply.github.com>
2022-05-21 23:49:59 +0100

    adding comments
    Also including variable lists

--- Klask.java
+++ Klask.java
@@ -1,36 +1,64 @@
 public class Klask{
+	// Generate the game arena
 	private GameArena game1 = new GameArena(800,500);
+	// Line borders for the Klask game
 	private Line left_line = new Line(100.0,100.0,100.0,400.0,15.0,"GREY",4);
 	private Line bottom_line = new Line(100.0,400.0,700.0,400.0,15.0,"GREY",4);
 	private Line right_line = new Line(700.0,400.0,700.0,100.0,15.0,"GREY",4);
 	private Line top_line = new Line(700.0,100.0,100.0,100.0,15.0,"GREY",4);
+	// Main game ball
 	private Ball the_ball = new Ball(350.0,250.0,10,"YELLOW",3);
+	// Generating the pucks left puck --v
 	private Ball left_puckball = new Ball(550.0,250.0,20,"BLACK",2);
 	private Line left_puckline = new Line(550.0,250.0,550.0,230.0,10,"BLACK",2);
-	private Rectangle background = new Rectangle(100.0,100.0,600.0,300.0,"BLUE",1);
+	// Right puck --v
 	private Ball right_puckball = new Ball(250.0,250.0,20,"BLACK",2);
 	private Line right_puckline = new Line(250.0,250.0,250.0,230.0,10,"BLACK",2);
+	// Making the quatercircles
 	private Ball topleft_ball = new Ball(100.0,100.0,100,"GREY",2);
 	private Ball bottomleft_ball = new Ball(100.0,400.0,100,"GREY",2);
 	private Ball topright_ball = new Ball(700.0,100.0,100,"GREY",2);
 	private Ball bottomright_ball = new Ball(700.0,400.0,100,"GREY",2);
+	// Creating the filling for the quatercircles
 	private Ball topleft_gap = new Ball(100.0,100.0,90,"BLUE",3);
 	private Ball bottomleft_gap = new Ball(100.0,400.0,90,"BLUE",3);
 	private Ball topright_gap = new Ball(700.0,100.0,90,"BLUE",3);
 	private Ball bottomright_gap = new Ball(700.0,400.0,90,"BLUE",3);
+	// Blue game background
+	private Rectangle background = new Rectangle(100.0,100.0,600.0,300.0,"BLUE",1);
+	// Creating the black borders to cover the corner circles
 	private Line border1 = new Line(0.0,0.0,0.0,500.0,190,"BLACK",5);
 	private Line border2 = new Line(0.0,500.0,800.0,500.0,190,"BLACK",5);
 	private Line border3 = new Line(800.0,500.0,800.0,0.0,190,"BLACK",5);
 	private Line border4 = new Line(800.0,0.0,0.0,0.0,190,"BLACK",5);
+	// Creating the goals
 	private Ball left_goal = new Ball(150.0,250.0,50,"GREY",2);
 	private Ball right_goal = new Ball(650.0,250.0,50,"GREY",2);
+	// Middle line through the game
 	private Line middle_line = new Line(400.0,0.0,400.0,500.0,0.5,"BLACK",2);
-	private Ball top_ball = new Ball(400.0,175.0,10,"WHITE",3);
-	private Ball middle_ball = new Ball(400.0,250.0,10,"WHITE",3);
-	private Ball bottom_ball = new Ball(400.0,325.0,10,"WHITE",3);
+	// Creating the middle magnets
+	private Ball top_magnet = new Ball(400.0,175.0,10,"WHITE",3);
+	private Ball middle_magnet = new Ball(400.0,250.0,10,"WHITE",3);
+	private Ball bottom_magnet = new Ball(400.0,325.0,10,"WHITE",3);
+
+/* UI instance variable list:
+	game1 = the game arena
+	left_line --> top_line = the borders
+	the_ball = the main game ball
+	left_puckball/left_puckline = creating the puck (replace with right)
+	topleft_gap etc. = creating the quater circles in the corners 
+	topleft_gap etc. = creatinf the insided of the corners 
+	background = the blue background
+	border1/2/3/4 = the black border (covers the corner circles)
+	left_goal/right_goal = player goals
+	middle_line = middle line through the game
+	top_megnet/middle/bottom = the magnets
+
+*/
 
 
 	public Klask(){
+		// Adding the UI instance variables to the gameboard
 		game1.addBall(the_ball);
 		game1.addLine(left_line);
 		game1.addLine(bottom_line);
@@ -56,9 +84,9 @@ public class Klask{
 		game1.addBall(left_goal);
 		game1.addBall(right_goal);
 		game1.addLine(middle_line);
-		game1.addBall(top_ball);
-		game1.addBall(middle_ball);
-		game1.addBall(bottom_ball);
-
+		game1.addBall(top_magnet);
+		game1.addBall(middle_magnet);
+		game1.addBall(bottom_magnet);
+		// end of the UI additions -----------------------
 	}
 }
