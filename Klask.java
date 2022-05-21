0c660b2428197786954a623699f0b5467b52b770
humphr18 <101128063+humphr18@users.noreply.github.com>
2022-05-21 21:30:23 +0100

    Creating gamearena view

---
+++ Klask.java
@@ -0,0 +1,7 @@
+public class Klask{
+	private GameArena game1 = new GameArena(500,500);
+
+	public Klask(){
+
+	}
+}
---
+++ newGame.java
@@ -0,0 +1,5 @@
+public class newGame{
+	public static void main(String[] args){
+		Klask playgame = new Klask();
+	}
+}
